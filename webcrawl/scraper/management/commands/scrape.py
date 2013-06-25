0e536783b6ac3e6a3fe8e7ea51f0044810060251
Doug <douglas.chau@dowjones.com>
2013-06-25 20:33:34 +0000

    Connects the webcrawler application to AWS database

--- webcrawl/scraper/management/commands/scrape.py
+++ webcrawl/scraper/management/commands/scrape.py
@@ -1,27 +1,39 @@
+<<<<<<< HEAD
 import urllib2, urlparse, sys, traceback, pdb, unicodedata, cookielib, time, threading
+=======
+import urllib2, urlparse, sys, traceback, unicodedata, cookielib, time
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management.base import make_option
 from scraper.models import Website
 
+<<<<<<< HEAD
 url = sys.argv[2]
 wait = float(sys.argv[3])
+=======
+url = sys.argv[1]
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 url_key = urlparse.urlsplit(url).netloc
 jar = cookielib.FileCookieJar("cookies")
 
 urls = [url]
 visited = {url : "1"}
 storage = []
+<<<<<<< HEAD
 threads = []
 urlArray = []
 dummy = "http://news.google.com/news/section?pz=1&cf=all&ned=us&hl=en&q=Wimbledon&topicsid=FRONTPAGE&ict=tnv6"
 
+=======
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		self.stdout.write('Successfully opened scrape.py')
 		main()
 
+<<<<<<< HEAD
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter, urlForScrape):
         threading.Thread.__init__(self)
@@ -77,6 +89,23 @@ def startScrapper(urlForScrape):
 	print '\n\n'
 	print time.time() - start_time, "seconds"
 
+=======
+def main():
+	start_time = time.time()
+	initiate()
+	try:
+		soup = openURL(url[0])
+	except Exception as ex:
+		print "Exception in user code: " + str(ex)
+		print "URL: " + urls[0]
+
+	storage = scrape(soup)
+
+	print storage
+	print time.time() - start_time, "seconds"
+
+#returns a string containing all text in html
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 def scrape(soup):
 	if soup.findAll(text = True):
 		holder = []
@@ -86,12 +115,19 @@ def scrape(soup):
 		return "".join(holder)
 
 def initiate():
+<<<<<<< HEAD
 	print "the length of arguments are %d" % len(sys.argv)
 	if len(sys.argv) < 3:
 		print "Input must be two arguments\nExiting..."
 		sys.exit()
 	elif sys.argv[2].find("://") == -1:
 		print sys.argv[2]
+=======
+	if len(sys.argv) != 3:
+		print "Input must be two arguments\nCurrently %d\nExiting..." % len(sys.argv)
+		sys.exit()
+	elif sys.argv[2].find("://") == -1:
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 		print "Input should be include scheme/protocol!\nExiting..."
 		sys.exit()
 
@@ -99,12 +135,18 @@ def openURL(url):
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
 	htmltext = BeautifulSoup(opener.open(urls[0]))
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 	#traceback.print_exc(file=sys.stdout) #prints entire stack trace
 	return htmltext
 
 def addLinks(soup):
+<<<<<<< HEAD
 	
+=======
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 	for tag in soup.findAll("a", href = True):
 		 	tag['href'] = urlparse.urljoin(url, tag['href'])
 		 	tag['href'].encode("utf-8")
@@ -114,7 +156,10 @@ def addLinks(soup):
 		 	if tag['href'].find(url_key) != -1 and not visited.has_key(tag['href'].encode("utf-8")):
 		 		urls.append(tag['href'].encode("utf-8"))
 		 		visited[tag['href'].encode("utf-8")] = "1"
+<<<<<<< HEAD
 	return urls	 
+=======
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
 
 if __name__ == "__main__":
 	main()
--- webcrawl/scraper/models.py
+++ webcrawl/scraper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+<<<<<<< HEAD
 from django.db.models.base import ModelBase
 # Create your models here.
 
@@ -8,3 +9,17 @@ class Website(models.Model):
 	modifiedTime = models.DateTimeField(auto_now = True)
 	def __unicode__(self):
 		return "URL: %s" % self.url
+=======
+
+# Create your models here.
+
+class Website(models.Model):
+	http = models.CharField(max_length = 200)
+	bodytext = models.TextField()
+	def __unicode__(self):
+		return self.http
+
+class Queries(models.Model):
+	user_id=models.CharField(max_length=200)
+	query=models.CharField(max_length=200)
+>>>>>>> 2e4fa8b70daf6a01bceaa93166b81401a3f7daf1
