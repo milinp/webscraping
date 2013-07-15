dccb6aad9020b901c4bf38e4f95eb6811b2481dc
Doug <douglas.chau@dowjones.com>
2013-07-15 14:37:52 +0000

    adds admins and client side applications

--- webcrawl/scraper/management/commands/masterScrape.py
+++ webcrawl/scraper/management/commands/masterScrape.py
@@ -8,25 +8,30 @@ import cookielib
 import time
 import threading
 import re
+import os, signal, subprocess
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management.base import make_option
-from scraper.models import Visited, URLToVisit
+from scraper.models import Visited, URLToVisit, DummyVisited
 
 url = ""
 wait = 0
 url_key = "fafa"
 jar = cookielib.FileCookieJar("cookies")
-
 urls = []
 visited = {url : "1"}
-threads = []
+isStop = False
 urlArray = []
+start_time = 0
 #dummy = "http://pastebin.com/ucDWguUf"
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		main(args)
+		startTimer()
+		if(args[0] == 'stop'):
+			stopScraping()
+		else:
+			main(args)
 
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter, urlForScrape):
@@ -40,7 +45,16 @@ class myThread (threading.Thread):
         startScrapper(self.urlForScrape)
 
 def main(args):
+	global isStop
+	isStop = False
+	global urlArray
 	global url
+	# URLToVisit.objects.all().delete()
+	# unfinishedURLData = URLToVisit.objects.all()
+	# if unfinishedURLData:
+	# 	for unfinishedURL in unfinishedURLData:
+	# 		urls.append(unfinishedURL.url)
+	# 		URLToVisit.objects.all().delete()
 	url = args[0]
 	urls.append(url)
 	global url_key
@@ -48,15 +62,30 @@ def main(args):
 	wait = float(args[1])
 	try:
 		soup = openURL(urls[0])
-		urlArray = addLinks(soup)
+		urlArray.extend(addLinks(soup))
 		i = 0
 		#print "length of URL array - %d" % (len(urlArray))
 		for urlForScrape in urlArray:
-			threadName = "Thread number %d" % (i)
-			thread = myThread(1, threadName, 1, urlForScrape)
-			thread.start()
-			thread.join()
-			# threads.append(thread)
+			print ("\n\n\tinside the for loop \n\n\n")
+			status = getStatus()
+			#print status
+			print getTime()
+			if status or getTime() > float(args[2]) - 2:
+				print("We killed it")
+				#sys.exit(0)
+				#break
+				
+				#threads.append(thread)
+			#	print ("\n\n length of threads = %d" % (len(threads)))
+			else: 
+				i = i + 1
+				threadName = "Thread number %d" % (i)
+				print ("Thread name = %s" % (threadName))
+				thread = myThread(1, threadName, i, urlForScrape)
+				time.sleep(wait)
+				thread.start()
+				thread.join()
+				urlArray.pop(0)
 	except Exception as ex:
 		print "Exception in user code: " + str(ex)
 		print "URL: " + urls[0]  
@@ -67,39 +96,65 @@ def main(args):
 
 	#print storage
 	#print time.time() - start_time, "seconds"
+def getStatus()	:
+	return isStop
 
 def startScrapper(urlForScrape):
 	#print "URL for Scraping = %s \n\n" % (urlForScrape)
 	start_time = time.time()
 	urlSoup = ""
-	# if not Visited.objects.filter(url = urlForScrape).exists():
-	urlSoup = openURL(urlForScrape)
-	urlArray.append(addLinks(urlSoup))
-	parsedText = scrape(urlSoup)
-	Visited(url = urlForScrape, urlData = parsedText).save()
+	if not DummyVisited.objects.filter(url = urlForScrape).exists():
+		print "new entry = %s\n\n" % (urlForScrape)
+		urlSoup = openURL(urlForScrape)
+
+		addLinks(urlSoup)
+		parsedText = scrape(urlSoup)
+		DummyVisited(url = urlForScrape, urlData = parsedText).save()
+		
+	else:
+		print "Repeated entry  = %s" % (urlForScrape)
 
 def scrape(soup):
 	if soup.findAll(text = True):
 		holder = []
 		for s in soup.findAll(text = True):
-			if "".join(s.encode("ascii", "ignore").split()):
-				holder.append("".join(s.encode("ascii", "ignore").split()))
-		return "".join(holder)
+			if " ".join(s.encode("ascii", "ignore").split()):
+				holder.append(" ".join(s.encode("ascii", "ignore").split()))
+		return " ".join(holder)
 
 def openURL(aURL):
-	time.sleep(wait)
+	#time.sleep(wait)
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
 	htmltext = BeautifulSoup(opener.open(aURL))
 	return htmltext
 
 def addLinks(soup):
-	time.sleep(wait)
+	#time.sleep(wait)
 	for tag in soup.findAll("a", href = True):
 		 	tag['href'] = urlparse.urljoin(url, tag['href'])
 		 	tag['href'].encode("utf-8")
 		 	#check to make sure crawler stays within page domain
 		 	#also make sure crawler does not put an already visited page into the queue
 		 	if tag['href'].find(url_key) != -1 and not visited.has_key(tag['href'].encode("utf-8")):
-		 		urls.append(tag['href'].encode("utf-8"))
+		 		urlArray.append(tag['href'].encode("utf-8"))
 		 		visited[tag['href'].encode("utf-8")] = "1"
 	return urls	 
+
+def stopScraping():
+	global isStop
+	global urlArray
+	print "STOP STOP STOP !!!"
+	print len(urlArray)
+	#urlArray = []
+	isStop = True
+	# for remainingUrl in urlArray:
+	# 	if not DummyVisited.objects.filter(url = remainingUrl).exists():
+	# 		URLToVisit(url = remainingUrl).save()
+	# 
+
+def startTimer():
+	global start_time
+	start_time = time.time()
+
+def getTime():
+	return time.time() - start_time
--- webcrawl/scraper/models.py
+++ webcrawl/scraper/models.py
@@ -1,3 +1,5 @@
+import os, re 
+import scraper
 from django.db import models
 from django.db.models.base import ModelBase
 # Create your models here.
@@ -5,6 +7,7 @@ from django.db.models.base import ModelBase
 class Visited(models.Model):
 	url = models.CharField(primary_key = True, max_length = 255)
 	urlData = models.TextField()
+	urlInfo = models.TextField()
 	modifiedTime = models.DateTimeField(auto_now = True)
 	def __unicode__(self):
 		return "URL: %s" % self.url
@@ -13,3 +16,21 @@ class URLToVisit(models.Model):
 	url = models.CharField(primary_key = True, max_length = 255)
 	def __unicode__(self):
 		return "URL: %s" % self.url
+
+class DummyVisited(models.Model):
+	url = models.CharField(primary_key = True, max_length = 255)
+	urlData = models.TextField()
+	urlInfo = models.TextField()
+	modifiedTime = models.DateTimeField(auto_now = True)
+	def __unicode__(self):
+		return "URL: %s" % self.url
+
+class Document(models.Model):
+	docfile = models.FileField(upload_to='Watchlists')
+	def __unicode__(self):
+		return self.docfile.name
+	# Used to get the path of specific watchlist to pass on to the readfile.py matching process
+	def path(self):
+		m = re.search('<Document: (.*?)>', str(self.path))
+		return os.path.dirname(os.path.abspath(scraper.__file__)) + "/media/" + str(m.group(1)) # MEDIA_ROOT + watchlist/name
+
