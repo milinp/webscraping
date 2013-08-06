d2ea4aab278f9d45c8545f6d6fce16fda7a2a363
Doug <douglas.chau@dowjones.com>
2013-08-06 16:29:37 -0500

    fixes client side UI to match DJ

---
+++ webcrawl_user/static/js/parallax.js
@@ -0,0 +1,27 @@
+function EasyPeasyParallax() {
+	scrollPos = $(this).scrollTop();
+	$('#banner').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+	$('#bannertext').css({
+		'margin-top': (scrollPos/4)+"px",
+		'opacity': 1-(scrollPos/250)
+	});
+}
+
+$(document).ready(function(){
+	$(window).scroll(function() {
+		EasyPeasyParallax();
+	});
+
+	$('#bannertext').hover(
+    function(){
+        $(this).toggleClass('shadow');
+    });
+
+	$("#bannertext").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#content").offset().top
+				}, 1000, 'swing');
+    });
+});
